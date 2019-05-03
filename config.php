23f6932e4f96b5f1c8dc1c47e4846a05caa084e7
admin <37464913+shivam3898@users.noreply.github.com>
2019-05-03 11:32:09 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$hostName = 'localhost';
+$userName = 'root';
+$password = '';
+$databaseName = 'mini';
+
+$mysqli = new mysqli($hostName, $userName, $password, $databaseName);
+
+if ($mysqli->connect_error){
+    echo "Connection Error....<br>";
+}
+else{
+    echo "Connected Successfully";
+}
+?>
